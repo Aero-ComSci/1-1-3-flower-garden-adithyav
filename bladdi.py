824137181264a866736cfe08ce185556e0d673ac
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2024-09-03 15:24:16 -0700

    Update bladdi.py

--- bladdi.py
+++ bladdi.py
@@ -1,54 +1,83 @@
 import turtle as trtl
-screen = trtl.Screen()
-screen.setup(800, 800)
 
-painter = trtl.Turtle()
-painter.shape("turtle")
-painter.color("Green")
-painter.speed(10)
+flowers_options = ["Daisy", "Lily", "Roses", "Marigold", "Tulip"] #avaiable flowers that the application can draw
+print(flowers_options)
 
-flowers = ["Daisy", "Lily", "Roses", "Marigold", "Tulip"] #avaiable flowers that the application can draw
+def draw_single_flower():
+            flower_type = input("What type of flower do you want me to draw?: ")
+            if flower_type in flowers_options and flower_type == "Daisy" or flower_type == "daisy".lower():
+                print("Sure, I will draw a daisy")
+                turtle = open(trtl)
+                screen = trtl.Screen()
+                painter = trtl.Turtle()
+                #flower petal
+                for flower in range(6):
+                    painter.pendown()
+                    painter.begin_fill()
+                    painter.color("pink")
+                    painter.circle(30)
+                    painter.right(60)
+                    painter.penup()
+                    painter.goto(0,0)
+                    painter.pendown()
+                    painter.end_fill()
+            #stem of the flower        
+            painter.penup()
+            painter.goto(0,-30)
+            painter.pendown()
+            painter.begin_fill()
+            painter.color("yellow")
+            painter.circle(30)
+            painter.end_fill()
 
-def daisy():
-    painter.pensize(13)
-    painter.penup()
-    painter.goto(0, -200) 
-    painter.pendown()
-    painter.begin_fill()
-    painter.fillcolor("green")
-    painter.color("green")
-    painter.right(-90)
-    painter.forward(200)
-    for i in range (6):
-        painter.speed(10)
-        painter.begin_fill()
-        painter.color("black")
-        painter.fillcolor("red")
-        painter.pensize(8)
-        painter.circle(40)
-        painter.right(60)
-        painter.end_fill()
+            painter.penup()
+            painter.goto(0,-60)
+            painter.pendown()
+            painter.pencolor("Green")
+            painter.pensize(10)
+            painter.right(90)
+            painter.forward(100)
+
+def draw_multiple_flowers(number_of_flowers):
+    for flower in range(number_of_flowers):
+        draw_single_flower()
+        screen = trtl.Screen()
+        painter = trtl.Turtle()
+
+        for i in range(6):
+            painter.pendown()
+            painter.begin_fill()
+            painter.color("pink")
+            painter.circle(30)
+            painter.right(60)
+            painter.penup()
+            painter.goto(0,0)
+            painter.pendown()
+            painter.end_fill()
+        
+        #stem of the flower        
         painter.penup()
-daisy()
-
-def lily():
-    painter.goto(100, -200)
-    painter.pendown()
-    painter.color("green")
-    painter.pensize(13)
-    painter.forward(250)
-    for i in range (6):
-        painter.speed(10)
+        painter.goto(0,-30)
+        painter.pendown()
         painter.begin_fill()
-        painter.color("black")
-        painter.fillcolor("")
-        painter.color("black")
-        painter.pensize(8)
-        painter.circle(40)
-        painter.right(60)
+        painter.color("yellow")
+        painter.circle(30)
         painter.end_fill()
-        painter.penup()
 
-lily()
+        painter.penup()
+        painter.goto(0,-60)
+        painter.pendown()
+        painter.pencolor("Green")
+        painter.pensize(10)
+        painter.right(90)
+        painter.forward(100)
+
+number_of_flowers_to_draw = int(input("How many flowers do you want me to draw?: "))
+draw_multiple_flowers(number_of_flowers_to_draw)
+
+trtl.hideturtle()
+trtl.done()
 wn = trtl.Screen()
 wn.mainloop()
+
+
